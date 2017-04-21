d121d30dac17d0273acecc8694b5aa896467503a
josepjgutierrez <josephjamesgutierrez@gmail.com>
2017-04-21 10:59:05 -0700

    User can view data pulled from API

--- src/app/music-collection.service.ts
+++ src/app/music-collection.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Jsonp } from '@angular/http';
+import { Jsonp } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 
@@ -10,17 +10,13 @@ import { Music } from './music';
 export class MusicService {
   private musicUrl = 'https://itunes.apple.com/search?term=jack+johnson&limit=5&callback=JSONP_CALLBACK';
 
-  constructor(private http: Http, private jsonp: Jsonp) {}
+  constructor(private jsonp: Jsonp) {}
 
   getAlbumCollection() {
     return this.jsonp.request(this.musicUrl, {method: 'Get'})
       .toPromise()
       .then(response => response.json())
       .catch(this.handleError);
-    // return this.http.get(this.musicUrl)
-    //   .toPromise()
-    //   .then(response => response.json() as Music[])
-    //   .catch(this.handleError);
   }
 
   private handleError(error: any): Promise<any> {
